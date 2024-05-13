b8553f91cf0a1700d503a9107e2c4f7338b4ce3a
mlaskowski7 <mateuszlaskowski@Mateuszs-Air.lan>
2024-05-14 01:08:01 +0200

    main menu new game funcitonality

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -30,28 +30,44 @@ public class MainMenu {
         Box header = Box.createVerticalBox();
         JLabel headerLabel = new JLabel("<html><div style='font-size:36px;  color: #FF00FF; text-align:center;'>PacMan Game</div></html>");
         JLabel authorLabel = new JLabel("<html><div style='font-size:16px; color: gray; text-align:center;'>By Mateusz Laskowski</div></html>");
+        JLabel highScoreLabel = new JLabel("<html><div style='font-size:16px; color: white; text-align:center;'>Your high Score: " + highScore + "</div></html>");
         headerLabel.setFont(font.deriveFont(Font.BOLD));
         authorLabel.setFont(font);
+        highScoreLabel.setFont(font);
         headerLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         authorLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        highScoreLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         header.add(headerLabel);
         header.add(authorLabel);
+        header.add(highScoreLabel);
 
         return header;
     }
 
-    public static JButton newGameButton(){
+    public static JButton newGameButton(JFrame window, JPanel menuPanel){
         JButton newGameButton = new JButton("New Game");
-        newGameButton.setFont(font.deriveFont(16f));
+        newGameButton.setFont(font.deriveFont(18f));
         newGameButton.setForeground(new Color(0xFF00FF));
         newGameButton.setBackground(Color.BLACK);
         newGameButton.setOpaque(false);
+
+        ActionListener newGameAction = new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                window.remove(menuPanel);
+                window.revalidate();
+                window.repaint();
+            }
+        };
+
+        newGameButton.addActionListener(newGameAction);
+
         return newGameButton;
     }
 
     public static JButton exitButton(){
         JButton exitButton = new JButton("Exit");
-        exitButton.setFont(font.deriveFont(16f));
+        exitButton.setFont(font.deriveFont(18f));
         exitButton.setForeground(new Color(0xFF00FF));
         exitButton.setBackground(Color.BLACK);
         exitButton.setOpaque(false);
@@ -71,29 +87,31 @@ public class MainMenu {
 //    method used to initialize main menu
     public static void init(JFrame window){
         Box headerBox = header();
-        JButton newGameButton = newGameButton();
-        JButton exitButton = exitButton();
         JPanel menuPanel = new JPanel(new GridBagLayout());
+        JButton newGameButton = newGameButton(window, menuPanel);
+        JButton exitButton = exitButton();
+
 
 //        Styling for the header box
         GridBagConstraints headerGridConstraints = new GridBagConstraints();
         headerGridConstraints.gridx = 0;
         headerGridConstraints.gridy = 0;
-        headerGridConstraints.anchor = GridBagConstraints.CENTER;
+        headerGridConstraints.anchor = GridBagConstraints.WEST;
+        headerGridConstraints.insets = new Insets(0, 0, 15, 0);
         menuPanel.add(headerBox, headerGridConstraints);
 
 //        Styling for new game button
         GridBagConstraints newGameGridConstraints = new GridBagConstraints();
         newGameGridConstraints.gridx = 0;
         newGameGridConstraints.gridy = 1;
-        newGameGridConstraints.anchor = GridBagConstraints.CENTER;
+        newGameGridConstraints.anchor = GridBagConstraints.WEST;
         menuPanel.add(newGameButton, newGameGridConstraints);
 
 //        Styling for exit button
         GridBagConstraints exitGridConstraints = new GridBagConstraints();
         exitGridConstraints.gridx = 0;
         exitGridConstraints.gridy = 2;
-        exitGridConstraints.anchor = GridBagConstraints.CENTER;
+        exitGridConstraints.anchor = GridBagConstraints.WEST;
         menuPanel.add(exitButton, exitGridConstraints);
 
         menuPanel.setBackground(Color.BLACK);
